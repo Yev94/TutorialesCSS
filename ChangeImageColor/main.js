2049dbcc483ece1d833a27e6ca42657e9320eb79
Yev <tjs_694@hotmail.com>
2022-02-23 14:26:48 +0100

    inicialized repository

---
+++ ChangeImageColor/main.js
@@ -0,0 +1,6 @@
+const bg = document.getElementById('bg');
+    
+function randomColor(){return Math.floor(Math.random()*255)}
+bg.addEventListener('click', () =>  {
+bg.style.backgroundColor = 'rgba(' + randomColor() + ', ' + randomColor() + ', ' + randomColor() + ')';
+})
